0ffbc7ad03a544f9e6733d579b358941f5b650c2
Denis S Dujota <denisdujota@gmail.com>
2024-12-23 20:39:09 +0300

    connect to mongoose and add fruit model

---
+++ models/fruit.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// 1.) create the schema
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  isReadyToEat: Boolean,
+});
+
+// 2.) register the model
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+// 3.)  share it with the rest of the app
+module.exports = Fruit;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-// Here is where we import modules
-// We begin by loading Express
+require('dotenv').config();
+require('./config/database');
+
 const express = require('express');
 const morgan = require('morgan');
+
+// Models
+const Fruit = require('./models/fruit.js');
 
 const app = express();
 
@@ -9,6 +13,9 @@ const app = express();
 app.use(morgan('dev'));
 
 // ROUTES
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
