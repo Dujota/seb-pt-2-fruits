335280e666192ac811e48b93e8d58a9853c19c91
Denis S Dujota <denisdujota@gmail.com>
2024-12-24 18:53:04 +0300

    add the create fruit and index flow

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 // MIDDLEWARE
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 app.get('/', async (req, res) => {
@@ -20,6 +21,23 @@ app.get('/', async (req, res) => {
 // GET /fruits/new
 app.get('/fruits/new', (req, res) => {
   res.render('fruits/new.ejs');
+});
+
+// GET /fruits
+app.get('/fruits', async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render('fruits/index.ejs', { fruits: allFruits });
+});
+
+// POST /fruits
+app.post('/fruits', async (req, res) => {
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect('/fruits');
 });
 
 app.listen(3000, () => {
